9f2fefa6011d9a6609bbf7b8d04897e09f77f362
senyugege <senyugege@gmail.com>
2026-03-24 16:49:47 +0800

    chore: remove temp files

--- push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\Administrator\Desktop\OpenClaw_ImageGen"
-git add -A
-git commit -m "update: add watermark removal tools (Lama + OpenCV), update SKILL.md workflow"
-git push
-echo Done
